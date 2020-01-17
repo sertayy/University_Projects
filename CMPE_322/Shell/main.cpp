068495cf9fedbe6b7b0012d17218e64d17281838
sertayy <44103341+sertayy@users.noreply.github.com>
2020-01-17 16:43:52 +0300

    Add files via upload

---
+++ CMPE_322/Shell/main.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string>
+#include <fcntl.h>
+#include <vector>
+
+void createfork(char **string);
+
+using namespace std;
+int main() {
+    char * user_name = getenv("USER");
+
+    vector<string> commands;
+    char* args[3];
+    string command;
+    string nwcommand;
+    string cmd;
+    string dash;
+
+    while(command.compare("exit")) {
+
+        cout << user_name;
+        cout << " >>> ";
+
+        getline(cin, command);
+        nwcommand = command.substr(0, command.find(" "));
+        commands.push_back(command);
+
+            if (command.find(" grep ")!= string::npos) {
+
+                string arg = command.substr(command.find("\"")+1);
+                arg = arg.substr(0, arg.find("\""));
+                int pipefd[2], status;
+                pid_t pid;
+                pipe(pipefd);
+                pid = fork();
+
+                if (pid == 0) {
+
+                    if(!command.substr(0,command.find("|")).compare("listdir ")){
+                        cout<<"girdi"<<endl;
+
+                        close(pipefd[0]);
+                        dup2(pipefd[1], STDOUT_FILENO);
+                        cmd = "ls";
+                        args[0] = (char*) cmd.c_str();
+                        args[1] = NULL;
+                        args[2] = NULL;
+                        execvp(args[0],args);
+                    }else if(!command.substr(0,command.find("|")).compare("listdir -a ")){
+                        close(pipefd[0]);
+                        dup2(pipefd[1], STDOUT_FILENO);
+                        cmd = "ls";
+                        dash = "-a";
+                        args[0] = (char *) cmd.c_str();
+                        args[1] = (char *) dash.c_str();
+                        args[2] = NULL;
+                        execvp(args[0],args);
+                    }
+                }
+
+                pid = fork();
+                if (pid == 0) {
+
+                    close(pipefd[1]);
+                    dup2(pipefd[0], STDIN_FILENO);
+                    cmd = "grep";
+                    args[0] = (char *) cmd.c_str();
+                    args[1] = (char *) arg.c_str();
+                    args[2] = NULL;
+                    execvp(args[0],args);
+                }
+                close(pipefd[0]);
+                close(pipefd[1]);
+                waitpid(-1, &status, 0);
+                waitpid(-1, &status, 0);
+            }
+
+        else if (!command.compare("listdir -a")) {
+
+            cmd = "ls";
+            dash = "-a";
+            args[0] = (char *) cmd.c_str();
+            args[1] = (char *) dash.c_str();
+            args[2] = NULL;
+            createfork(args);
+
+        } else if (!nwcommand.compare("listdir")) {
+
+            cmd = "ls";
+            args[0] = (char *) cmd.c_str();
+            args[1] = NULL;
+            args[2] = NULL;
+            createfork(args);
+
+        } else if (!nwcommand.compare("currentpath")) {
+
+            cmd = "pwd";
+            args[0] = (char *) cmd.c_str();
+            args[1] = NULL;
+            args[2] = NULL;
+            createfork(args);
+
+        } else if (!nwcommand.compare("printfile")) {
+
+            string file = command.substr(10);
+            cmd = "cat";
+            args[0] = (char *) cmd.c_str();
+            args[1] = (char *) file.c_str();
+            args[2] = NULL;
+            createfork(args);
+            cout << endl;
+
+        } else if (!nwcommand.compare("Printfile")) {
+
+            command = command.substr(10);
+            string arg = command.substr(0, command.find(" "));
+            string arg2 = command.substr(command.find(">") + 2);
+
+            cmd = "cat";
+            args[0] = (char *) cmd.c_str();
+            args[1] = (char *) arg.c_str();
+            args[2] = NULL;
+
+            pid_t pid = fork();
+            if (pid == 0) {
+                int ofd;
+                ofd = creat(arg2.c_str(), 0644);
+                dup2(ofd, 1);
+                execvp(args[0], args);
+            } else {
+                wait(0);
+            }
+        } else if (!nwcommand.compare("footprint")) {
+
+            int line = 0;
+            if (commands.size() < 15) {
+                for (int i = 0; i <= commands.size() - 1; i++) {
+                    line++;
+                    cout << line;
+                    cout << +" " + commands.at(i) << endl;
+                }
+            } else {
+                for (int i = commands.size() - 15; i <= commands.size() - 1; i++) {
+                    line++;
+                    cout << line;
+                    cout << +" " + commands.at(i) << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+void createfork(char* args[3]){
+
+    pid_t pid = fork();
+    if(pid == 0){
+        if(execvp(args[0],args) == -1){
+            perror("exec");
+        }
+    }
+    if(pid > 0){
+        wait(0);
+    }
+}
---
+++ CMPE_322/Simulation/main.cpp
@@ -0,0 +1,149 @@
+/*
+ * Implemented by Sertay Akpinar, 02.01.2020
+ */
+
+#include <iostream>
+#include <pthread.h>
+#include <fstream>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+#include <queue>
+using namespace std;
+void *atm_threads(void *param);
+void *customer_threads(void *param);
+int number_of_cust; //number of the customers
+int electric;
+int gas;
+int cableTv;
+int telecom;
+int water;
+int max_sleep;  //maximum sleep time among the customers
+pthread_mutex_t atm_ = PTHREAD_MUTEX_INITIALIZER;   //mutex for atm
+pthread_mutex_t cust = PTHREAD_MUTEX_INITIALIZER;   //mutex for customer
+pthread_mutex_t g = PTHREAD_MUTEX_INITIALIZER; //mutex for gas
+pthread_mutex_t cable = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t elec = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t wat = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t tele = PTHREAD_MUTEX_INITIALIZER;
+
+struct customer{        //necessary data for customer
+    int sleep;
+    string billtype;
+    int atm_instance;
+    int amount;
+    int index;
+};
+
+struct atm{         //necessary data for atm
+    queue<customer> atm_queue;
+    int ind;
+};
+ofstream output;
+atm atm_array[10];  //queue array
+
+int main(int argc, char* argv[]) {
+    string line;
+    ifstream infile(argv[1]); //one for customer data
+    ifstream in1file(argv[1]);  // one for finding the maximum sleep among the customers
+    string file = argv[1];
+    file = file.substr(0,file.find("."));
+    file.append("_log.txt");    //to create the <input_name>_log.txt file
+    output.open(file);
+    infile >> number_of_cust;
+    pthread_t atms[10];
+    //I found the maximum sleep time among the customers via this for loop
+    max_sleep =0;
+    for(int i=0; i<number_of_cust; i++){
+        in1file >> line;
+        if(max_sleep < stoi(line.substr(0,line.find(",")))){
+            max_sleep = stoi(line.substr(0,line.find(",")));
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        atm_array[i].ind = i;
+        pthread_create(&atms[i], 0, atm_threads, &atm_array[i].ind);    //creates the atm threads and sends the each atms index's address
+    }
+    pthread_t customs[number_of_cust];
+    customer customers[number_of_cust];
+    //In the first for loop implemented below, I identified each customer's data according to the input file by using substr()
+    //after that I created the customer thread and send each customer's address
+    for(int i=0; i<number_of_cust;i++) {
+        infile >> line;
+        string temporary;
+        customers[i].index = i+1;
+        customers[i].sleep = stoi(line.substr(0, line.find(",")));
+        temporary = line.substr(0, line.find(","));
+        line = line.substr(temporary.size()+1);
+        if(max_sleep<customers[i].sleep){
+            max_sleep = customers[i].sleep;
+        }
+        customers[i].atm_instance = stoi(line.substr(0, line.find(",")));
+        temporary = line.substr(0, line.find(","));
+        line = line.substr(temporary.size()+1);
+        customers[i].billtype = line.substr(0, line.find(","));
+        temporary = line.substr(0, line.find(","));
+        line = line.substr(temporary.size()+1);
+        customers[i].amount = stoi(line.substr(0, line.find(",")));
+        pthread_create(&customs[i], 0, customer_threads, &customers[i]);
+    }
+    for (int i = 0; i < 10; i++) {
+        pthread_join(atms[i], NULL);
+    }
+    for(int i=0; i<number_of_cust;i++) {
+        pthread_join(customs[i],NULL);
+    }
+    output<<"All payments are completed"<<endl;
+    output<<"CableTV:"<< cableTv <<endl;
+    output<<"Electricity:"<< electric <<endl;
+    output<<"Gas:" << gas<< endl;
+    output<<"Telecommunication:" << telecom << endl;
+    output<<"Water:" << water;
+    return 0;
+}
+
+void *customer_threads(void *param){ //creates customer according to the *param, (I've sent each customer's address in the main function)
+    customer customers1 = *(customer *) param;
+    this_thread::sleep_for(chrono::milliseconds(customers1.sleep));//each customer sleeps according to the sleep time
+    int index = customers1.atm_instance;
+    int temp = index -1 ;
+    atm_array[temp].atm_queue.push(customers1); //I want one customer thread at a time while pushing the customers to the queue array
+    pthread_exit(0);    //exits after making its job
+}
+
+void *atm_threads(void *param){
+    int atm_index = *(int *) param;     // matches the atm_index with the *param, (I've sent each atms index's address in the main function)
+    //This sleep function is for protecting the queue accesses between the atm and customer threads
+    //In other words, since atm threads may access the queues before the customer pushed the queue,
+    //I want block the atm threads from accessing the queue for a while
+    this_thread::sleep_for(chrono::milliseconds(max_sleep+2000));
+    while(!atm_array[atm_index].atm_queue.empty()){
+        customer head_cust = atm_array[atm_index].atm_queue.front();    //the first customer who will pay his bill
+        // In the if-else block mentioned below, I calculated the each bill type
+        if(!head_cust.billtype.compare("cableTV")){
+            pthread_mutex_lock(&cable);
+            cableTv += head_cust.amount;
+            pthread_mutex_unlock(&cable);
+        } else if(!head_cust.billtype.compare("electricity")){
+            pthread_mutex_lock(&elec);
+            electric += head_cust.amount;
+            pthread_mutex_unlock(&elec);
+        } else if(!head_cust.billtype.compare("gas")){
+            pthread_mutex_lock(&g);
+            gas += head_cust.amount;
+            pthread_mutex_unlock(&g);
+        } else if(!head_cust.billtype.compare("water")){
+            pthread_mutex_lock(&wat);
+            water += head_cust.amount;
+            pthread_mutex_unlock(&wat);
+        } else if(!head_cust.billtype.compare("telecommunication")){
+            pthread_mutex_lock(&tele);
+            telecom += head_cust.amount;
+            pthread_mutex_unlock(&tele);
+        }
+        pthread_mutex_lock(&atm_);  //I want one thread at a time
+        output << "Customer" << head_cust.index<<","<<head_cust.amount << "TL," << head_cust.billtype << endl;
+        atm_array[atm_index].atm_queue.pop();   //pops the customer from the atm's atm_queue after he pays his bill
+        pthread_mutex_unlock(&atm_);
+    }
+    pthread_exit(0);     //exits after making its job
+}
